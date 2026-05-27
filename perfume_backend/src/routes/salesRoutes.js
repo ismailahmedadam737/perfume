bd387fee17f754d80b98899489e1cfc001719cc5
kamaalismailahmed-cmyk <email-kaaga-github@tusaale.com>
2026-05-27 22:51:03 +0300

    Waxaan saxay Login logic-ga

--- perfume_backend/src/routes/salesRoutes.js
+++ perfume_backend/src/routes/salesRoutes.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const salesController = require('../controllers/salesController');
 
+// Routes-ku waxay ka bilaabanayaan /api/sales (oo lagu qeexay app.js)
+// POST: /api/sales
 router.post('/', salesController.addSale);
+
+// GET: /api/sales
 router.get('/', salesController.getSales);
 
 module.exports = router;
