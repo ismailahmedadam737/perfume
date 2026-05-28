830556f6d36068016bd8472256165470bb0502fa
kamaalismailahmed-cmyk <email-kaaga-github@tusaale.com>
2026-05-28 07:57:28 +0300

    Waxaan saxay Login logic-ga

--- perfume_backend/src/models/settingModels.js
+++ perfume_backend/src/models/settingModels.js
@@ -1,5 +1,6 @@
 const pool = require('../config/db');
 
+// 1. Soo qaado xogta dukaanka
 const getSettings = async () => {
     try {
         const res = await pool.query('SELECT * FROM shop_settings WHERE id = 1 LIMIT 1');
@@ -10,9 +11,12 @@ const getSettings = async () => {
     }
 };
 
+// 2. Keydi ama Update garee xogta (UPSERT)
 const saveSettings = async (shopName, currencyName, phone, webLink, social, logoData, isRegistered) => {
     try {
-        // Hubi in column-yada ay u qoran yihiin sida ay database-ka ugu qoran yihiin
+        // Waxaan ka saarnay calaamadaha (") waxaana isticmaalnay magacyo yaryar oo dhan.
+        // HADDII database-kaaga uu ku jiro "shop_name", "currency_name" iwm, 
+        // fadlan magacyada hoose u beddel kuwa database-kaaga.
         const query = `
             INSERT INTO shop_settings (id, shopname, currencyname, phone, weblink, social, logodata, isregistered)
             VALUES (1, $1, $2, $3, $4, $5, $6, $7)
@@ -37,4 +41,7 @@ const saveSettings = async (shopName, currencyName, phone, webLink, social, logo
     }
 };
 
-module.exports = { getSettings, saveSettings };
+module.exports = { 
+    getSettings, 
+    saveSettings 
+};
