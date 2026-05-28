e6d08490ff8e8a86c08d0a022e581e7238c7ec56
kamaalismailahmed-cmyk <email-kaaga-github@tusaale.com>
2026-05-28 06:31:40 +0300

    Waxaan saxay Login logic-ga

--- perfume_backend/src/routes/customerRoutes.js
+++ perfume_backend/src/routes/customerRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const customerController = require('../controllers/customerController');
 
-// Waxaan isticmaalaynaa '/all' si ay ula jaanqaado API-yadaada kale
-router.get('/all', customerController.getAllCustomers);
-
-// Waxaan isticmaalaynaa '/add' si ay ula jaanqaado habka POST-ka ee kharashka
+// Halkan waa meesha dad badani ka qaldamaan
+// Haddii index.js uu yahay '/api/customers'
+// Markaas route-kan hoose wuxuu noqonayaa '/api/customers/all'
+router.get('/all', customerController.getAllCustomers); 
 router.post('/add', customerController.createCustomer);
 
 module.exports = router;
