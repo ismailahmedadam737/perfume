09e673138c9c074c8e35965deb35446229f7c304
kamaalismailahmed-cmyk <email-kaaga-github@tusaale.com>
2026-05-28 06:58:49 +0300

    Waxaan saxay Login logic-ga

--- perfume_backend/src/models/dashboardModel.js
+++ perfume_backend/src/models/dashboardModel.js
@@ -3,12 +3,13 @@ const pool = require('../config/db');
 const DashboardModel = {
     getCounts: async () => {
         try {
+            // Waxaan ku darnay "public." hortooda
             const query = `
                 SELECT 
-                    (SELECT COUNT(*) FROM products) AS total_products,
-                    (SELECT COUNT(*) FROM employees) AS total_employees,
-                    (SELECT COUNT(*) FROM customers) AS total_customers,
-                    (SELECT COUNT(*) FROM shop_settings) AS total_settings
+                    (SELECT COUNT(*) FROM public.products) AS total_products,
+                    (SELECT COUNT(*) FROM public.employees) AS total_employees,
+                    (SELECT COUNT(*) FROM public.customers) AS total_customers,
+                    (SELECT COUNT(*) FROM public.shop_settings) AS total_settings
             `;
             const result = await pool.query(query);
             return result.rows[0];
